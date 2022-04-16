8b08c806ce656319c69fa25c91222a51d4915bc0
Alexandre Fernandez <contact@alexandrefernandez.me>
2022-04-16 15:57:09 +0200

    added pointer handling for label

--- src/components/inputs/pieces/InputLabel.vue
+++ src/components/inputs/pieces/InputLabel.vue
@@ -4,12 +4,18 @@ import { defineComponent } from "vue"
 export default defineComponent({
 	props: {
 		required: Boolean,
+		isDisabled: Boolean,
 	},
 })
 </script>
 
 <template>
-	<label class="inline-block pl-4 mb-[3px]">
+	<label
+		:class="[
+			'inline-block pl-4 mb-[3px]',
+			$attrs.for && !isDisabled ? 'cursor-pointer' : '',
+		]"
+	>
 		<slot></slot>
 		<span v-if="required" class="text-danger-700"> * </span>
 	</label>
